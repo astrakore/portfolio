39b42ac5feec7a1d7ebe536bf78df4ef310c9ec4
astrakore <teresa.carrasco.sauri@gmail.com>
2022-09-15 09:35:37 +0200

    Primer commit clases generales CSS y media queries creadas

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from "./components/Header/Header"
+import Nav from "./components/Nav/Nav"
+import Skills from "./components/Skills/Skills"
+import Portfolio from "./components/Portfolio/Portfolio"
+import Contact from "./components/Contact/Contact"
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Skills />
+      <Portfolio />
+      <Contact />
+    </>
+  )
+}
+
+export default App
