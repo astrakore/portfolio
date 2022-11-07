92b1abb854eda0c00cb98012ce0f719c2cb48369
astrakore <teresa.carrasco.sauri@gmail.com>
2022-11-07 12:22:23 +0100

    Terminado Footer y cambiado color Nav

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import "./footer.css"
 
+import {BsLinkedin} from "react-icons/bs"
+import {BsTwitter} from "react-icons/bs"
+import {BsGithub} from "react-icons/bs"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <ul className='permalinks'>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#about">Sobre mí</a></li>
+        <li><a href="#skills">Habilidades</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contacto</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+      <a href='https://www.linkedin.com/in/teresa-carrasco/' target="_blank"><BsLinkedin/></a>
+        <a href='https://twitter.com/TereC_Sauri' target="_blank"><BsTwitter/></a>
+        <a href='https://github.com/astrakore' target="_blank"><BsGithub/></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; Teresa Carrasco. Todos los derechos reservados.</small>
+      </div>
+    </footer>
   )
 }
 
