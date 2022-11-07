592b27fcba2136dc52520941de8f9d69514778d1
astrakore <teresa.carrasco.sauri@gmail.com>
2022-11-07 10:59:09 +0100

    Sección de portfolio terminada arreglados detalles menores en header comenzada sección contacto

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import "./contact.css";
+import {MdOutlineMail} from "react-icons/md"
+import {BsWhatsapp} from "react-icons/bs"
 
 const Contact = () => {
   return (
-    <section id="contact">Contact</section>
+    <section className='contact' id="contact">
+      <h5>Contáctame</h5>
+      <h2>Contacto</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+
+          <article className='contact__option'>
+            <MdOutlineMail/>
+            <h4>Email</h4>
+            <h5>teresa.carrasco.sauri@gmail.com</h5>
+            <a href="mailto:teresa.carrasco.sauri@gmail.com" target="_blank">Envía un mensaje</a>
+          </article>
+
+          <article className='contact__option'>
+            <BsWhatsapp/>
+            <h4>WhatsApp</h4>
+            <h5>+34622560908</h5>
+            <a href="https://api.whatsapp.com/send?phone=+34622560908" target="_blank">Envía un mensaje</a>
+          </article>
+
+        </div>
+
+        <form action=''>
+          <input type="text" name="Nombre" placeholder="Tu nombre completo" required />
+          <input type="email" name="Correo electrónico" placeholder="Tu correo electrónico" required />
+          <textarea type="message" rows="7" name="Mensaje" placeholder="Escribe aquí tu mensaje" required></textarea>
+          <button type="submit" className='btn btn-primary'>Enviar</button>
+        </form>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import "./portfolio.css"
+import IMG1 from '../../assets/portfolio01.PNG'
+import IMG2 from '../../assets/portfolio02.PNG'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "deltaFight",
+    github: "https://github.com/astrakore/deltaFight",
+    demo: "https://astrakore.github.io/deltaFight/"
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Family Video",
+    github: "https://github.com/astrakore/frontalFamilyVideo",
+    demo: "https://master.d2ag7aygvmn19a.amplifyapp.com/"
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section className="portfolio" id="portfolio">
+    
+      <h5>Mis trabajos</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portoflio__item'>
+              <div className='portfolio__item-image'>
+                <img src={image} alt={title}/>
+              </div>
+              <h3>{title}</h3>
+              <div className='portfolio__item-cta'>
+                <a href={github} className='btn'>Github</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Proyecto</a>
+              </div>
+              </article>
+            )
+          })
+        }
+
+      </div>
+
+    </section>
   )
 }
 
